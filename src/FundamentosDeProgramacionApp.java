da05e1cca7f0d217b347e5e0813050c49d14152d
Carlos Ruhal Sebastian Ruiz Perez <sebastianperezruiz6@gmail.com>
2025-04-04 08:06:35 -0600

    Se cambio el codigo completo por que me equivoque y subi otro :(

--- src/FundamentosDeProgramacionApp.java
+++ src/FundamentosDeProgramacionApp.java
@@ -1,86 +1,106 @@
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Iterator;
-import java.util.List;
-import java.util.Vector;
+import java.util.Stack;
+
+
+
+/*
+ *  push() para meter datos en la pila
+	pop() elimina el ultimo valor de la pila  y retorna el mismo
+	peek( ) Returns the top element of the stack.
+	top() Returns the top element of the stack.
+	isEmpty() returns true if stack is empty else false.
+	isFull() returns true if the stack is full else false
+ */
 
 public class FundamentosDeProgramacionApp {
-
+	/*
+	 * Este es el metodo encargado cuando se compila
+	 * java.
+	 * */
 	public static void main(String[] args) {
-		
-//		List<String> listaNombres = new ArrayList<>();
-//		listaNombres.add("carlos");
-//		listaNombres.add("ruhal");
-//		listaNombres.add("linda");
-//		listaNombres.add("mateo");
-//		listaNombres.add("kevin");
-//
-//		
-//		List<String> listaApellidosM = new ArrayList<>();
-//		listaApellidosM.add("ruiz");
-//		listaApellidosM.add("martinez");
-//		listaApellidosM.add("juarez");
-//		listaApellidosM.add("miranda");
-//
-//		List<String> listaApellidosP = new ArrayList<>();
-//		listaApellidosP.add("perez");
-//		listaApellidosP.add("lopez");
-//		listaApellidosP.add("luna");
-//		listaApellidosP.add("pacheco");
-//		
-//		int tamanioListaNombres= listaNombres.size();
-//		int tamanioListaApellidosM= listaApellidosM.size();
-//
-//		int tamanioListaApellidosP= listaApellidosP.size();
-//		
-//		System.out.println(tamanioListaNombres);
-//		System.out.println(tamanioListaApellidosM);
-//		System.out.println(tamanioListaApellidosP);
-//
-//
-//		
-//		for (int i = 0; i < listaNombres.size(); i++) {
-//			System.out.println(listaNombres.get(i));
-//			System.out.println(listaApellidosM.get(i));
-//			System.out.println(listaApellidosP.get(i));
-//
-//			
-//		}
-//		
-		
-//		7:00
-//		7:10
-//		
-//		7:10:01 > retardo <7:30:00
-//		3 o mas retardos impriman hola
-//		>7:30:00 falta
-		
-		//["10:23:21","10:30:30","7:09:12",,]
-		String hora1="10|:|2|3|:|2|1|";
+		Stack<Integer> pila1  = new Stack<>();
+		Stack<Integer> pilaAUX  = new Stack<>();
 	
-		List<Integer> listaNumeros = new ArrayList<>(List.of(4,7,86655,6575));
-		List<Integer> listaNumerosNuevos = null;
-		for (int i = 0; i < listaNumeros.size(); i++) {
-			
-			int numeroActual= listaNumeros.get(i);
-			listaNumerosNuevos = new ArrayList<>();
-			
-			
-			for(;numeroActual != 1;) {
-				//If Ternario
-				numeroActual =(numeroActual%2 != 0) ? numeroActual*3+1:numeroActual/2;
-				
-				if(numeroActual%2 != 0) {//impar
-					numeroActual = numeroActual*3+1;
-				}else {
-					numeroActual = numeroActual/2;
-				}
-				
-				listaNumerosNuevos.add(numeroActual);
+	pila1.push(4);
+	pila1.push(3);
+	pila1.push(2);
+	pila1.push(1);
 
+		
+//		Integer elemento;
+//		do {
+//			elemento = pila1.pop();
+//			pilaAUX.push(elemento);
+		//	if(elemento %2 == 0) {
+		//	pilaAUX.push(0);
+		//	
+		//}
+//			
+//		}while(!pila1.empty());
+		
+		//Segundo forma
+		
+//		Integer elemento;
+//		while(!pila1.empty()) {
+//			elemento = pila1.pop();
+//			pilaAUX.push(elemento);
+//			if(elemento %2 == 0) {
+//				pilaAUX.push(0);
+//				
+//			}
+//		};
+		
+		//tercera forma
+		Integer elemento;
+		Iterator iterator = pila1.iterator();
+		for (; iterator.hasNext();) {
+			 elemento = pila1.pop();
+			pilaAUX.push(elemento);
+			if(elemento %2 == 0) {
+				pilaAUX.push(0);
+				
 			}
 			
-			System.out.println(listaNumerosNuevos);
 		}
+		
+		System.out.println(pila1);
+		System.out.println(pilaAUX);	
+			
+	
+		
+		
+//		for (Integer elemento : pila1) {
+//			//valorTopAnterior =pila1.pop();
+//			
+//			pilaAUX.push(elemento);
+//			
+//			if(elemento %2 == 0) {
+//				pilaAUX.push(0);
+//				
+//			}
+//			System.out.println(pilaAUX);
+//			
+//		}
+//		pila1.clear();
+//		System.out.println(pila1);
+//		
+//		for (Integer elemento : pilaAUX) {
+//			pila1.push(elemento);
+//		
+//			
+//		}
+//		pilaAUX.clear();
+//		System.out.println(pilaAUX);
+//		System.out.println(pila1);
+//		
+//		
+//	}
+	
+		
 	}
-}
+
+
+
+		
+
+	}
